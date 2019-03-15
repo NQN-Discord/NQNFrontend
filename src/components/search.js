79c94e6f1295dc50f4caafd029cdc7e52cce433b
Blue <redacted>
2019-03-15 22:28:18 +0000

    Search box actually searches

--- src/components/search.js
+++ src/components/search.js
@@ -3,17 +3,43 @@ import React from "react";
 
 import connect from "react-redux/es/connect/connect";
 
+import {search} from "../actions/search";
+
 
 class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ""
+        }
+    }
+
     render() {
-        return (<p>Search box.</p>);
+        return (
+            <div>
+                <input
+                    type="text"
+                    onKeyPress={(event) => this.onKeyPress(event)}
+                />
+            </div>
+        );
+    }
+
+    onKeyPress(event) {
+        if (event.key === "Enter") {
+            this.props.search(event.target.value, 0, 20);
+        }
     }
 
 }
 
 const mapStateToProps = (state) => {};
 
-const mapDispatchToProps = dispatch => {};
+const mapDispatchToProps = dispatch => {
+    return {
+        search: (term, page, per_page) => dispatch(search(term, page, per_page))
+    };
+};
 
 export default connect(
     mapStateToProps,
