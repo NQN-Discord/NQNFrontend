e76da5b912308ab8fd7fb06a8495cc273d5ea794
Blue <redacted>
2019-03-16 11:52:14 +0000

    Clicking the image in a large result opens the full version

--- src/components/search_result_large.js
+++ src/components/search_result_large.js
@@ -8,13 +8,17 @@ export default function SearchResultLarge(props) {
     const md5 = post.md5;
     const ext = post.ext;
     const postSrc = `https://static1.e621.net/data/${md5.slice(0, 2)}/${md5.slice(2, 4)}/${md5}.${ext}`;
+    const e6page = `https://e621.net/post/show/${post.id}`
     return (
         <div className="page_result_large">
-            <img
-                src={postSrc}
-                alt=""
-            />
+            <a href={postSrc} target="_blank" rel="noopener noreferrer">
+                <img
+                    src={postSrc}
+                    alt=""
+                />
+            </a>
             <p>
+                <a href={e6page} target="_blank" rel="noopener noreferrer">View on e621</a><br/>
                 Artist: {post.artist}<br/>
                 Score: {post.score}<br/>
                 Favourites: {post.fav_count}<br/>
