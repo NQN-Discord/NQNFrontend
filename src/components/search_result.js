71c0ea92a52d6bdd82c46be2894f9a8600056f44
Blue <redacted>
2019-03-16 16:36:14 +0000

    Rescale properly

--- src/components/search_result.js
+++ src/components/search_result.js
@@ -11,6 +11,7 @@ export default class SearchResult extends Component {
 
         this.state = {
             previewType: "none",
+            attrs: {style: {}},
             src: props.post.ext === "swf"? swfPreview: previewSrc,
             replacement: null
         };
@@ -21,58 +22,116 @@ export default class SearchResult extends Component {
         return `${md5.slice(0, 2)}/${md5.slice(2, 4)}/${md5}`
     };
 
-    updateImage = () => {
+    elementSize = () => {
+        const g = document.getElementsByTagName('body')[0];
+        const x = window.innerWidth || document.documentElement.clientWidth || g.clientWidth;
+        const y = window.innerHeight|| document.documentElement.clientHeight|| g.clientHeight;
+        return {
+            width: (x * 30) / 100,
+            height: (y * 30) / 100
+        }
+    };
+
+    updateImage = (e) => {
+        let attrs = {};
+        if (e !== undefined) {
+            const windowHeight=window.innerHeight;
+            const windowWidth=window.innerWidth;
+            const width = e.target.naturalWidth;
+            const height = e.target.naturalHeight;
+            if ((width / height) > (windowWidth / windowHeight)) {
+                attrs = {style: {width: "30vw"}, width, height};
+            }
+            else {
+                attrs = {style: {height: "100%"}, width, height};
+            }
+        } else if (Object.keys(this.state.attrs).length !== 0) {
+            attrs = this.state.attrs;
+        }
         const sampleURL = `https://static1.e621.net/data/sample/${this.getURL()}.${this.props.post.ext}`;
         const fullURL = `https://static1.e621.net/data/${this.getURL()}.${this.props.post.ext}`;
         if (["none", "preview"].includes(this.state.previewType)) {
             if (this.props.isVisible) {
-                var replacement = new Image();
-                replacement.onload = () => {
-                    this.setState({
-                        previewType: "sample",
-                        src: sampleURL,
-                        replacement: null
-                    });
-                };
-                replacement.onerror = () => {
-                    replacement = new Image();
-                    replacement.onload = () => {
-                        this.setState({
-                            previewType: "full",
-                            src: fullURL,
-                            replacement: null
-                        });
-                    };
-                    replacement.src = fullURL;
-                };
-                replacement.src = sampleURL;
                 if (this.state.replacement === null) {
-                    this.setState(update(this.state, {
+                    const elementSize = this.elementSize();
+                    const largest = Object.keys(attrs.style)[0];
+                    if (attrs[largest] * 2 < elementSize[largest]) {
+                        var replacement = new Image();
+                        replacement.onload = () => {
+                            this.setState(update(this.state, {
+                                $merge: {
+                                    previewType: "sample",
+                                    src: sampleURL,
+                                    replacement: null
+                                }
+                            }));
+                        };
+                        replacement.onerror = () => {
+                            replacement = new Image();
+                            replacement.onload = () => {
+                                this.setState(update(this.state, {
+                                    $merge: {
+                                        previewType: "full",
+                                        src: fullURL,
+                                        replacement: null
+                                    }
+                                }));
+                            };
+                            replacement.src = fullURL;
+                        };
+                        replacement.src = sampleURL;
+                        this.setState(update(this.state, {
                             $merge: {
                                 previewType: "preview",
-                                replacement
+                                replacement,
+                                attrs: attrs
                             }
+                        }));
+                    }
+                    else if (this.state.previewType === "none") {
+                        this.setState(update(this.state, {
+                            $merge: {
+                                previewType: "preview",
+                                attrs: attrs
+                            }
+                        }));
+                    }
+                }
+                else if (Object.keys(this.state.attrs).length === 0) {
+                    this.setState(update(this.state, {
+                        $merge: {
+                            attrs: attrs
+                        }
                     }));
                 }
             }
             else if (this.state.previewType === "none") {
                 this.setState(update(this.state, {
-                        $merge: {
-                            previewType: "preview"
-                        }
+                    $merge: {
+                        previewType: "preview",
+                        replacement: null,
+                        attrs: attrs
+                    }
                 }));
             }
         }
     };
 
     componentDidUpdate() {
-        if (!this.props.isVisible && this.state.replacement !== null) {
-            this.setState(update(this.state,
-                {$merge: {
-                    replacement: null
-            }}));
+        if (!this.props.isVisible) {
+            if (this.state.replacement !== null) {
+                const replacement = this.state.replacement;
+                replacement.onerror = null;
+                replacement.src = "";
+                this.setState(update(this.state,
+                    {
+                        $merge: {
+                            replacement: null
+                        }
+                    }));
+            }
         }
-        if (this.props.isVisible && this.state.replacement === null && this.state.previewType === "preview") {
+        else if (this.state.previewType === "preview") {
             this.updateImage();
         }
     }
@@ -82,11 +141,12 @@ export default class SearchResult extends Component {
             <div className="page_result">
                 <img
                     src={this.state.src}
+                    style={this.state.attrs.style}
                     alt=""
                     onClick={() => {
                         this.props.onClick(this.props.post)
                     }}
-                    onLoad={() => {this.updateImage()}}
+                    onLoad={(e) => {this.updateImage(e)}}
                 />
             </div>
         );
