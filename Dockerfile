a3bd0c7566d939fb6f141f8490e70d714e815f44
Simon <u1704428@warwick.ac.uk>
2019-03-22 15:49:31 +0000

    Update for Docker related changes

--- Dockerfile
+++ Dockerfile
@@ -14,4 +14,5 @@ RUN npm run build
 
 
 FROM nginx:1.15
-COPY --from=build /usr/src/app/build/static /usr/share/nginx/html
+COPY --from=build /usr/src/app/build /usr/share/nginx/html
+COPY nginx.conf /etc/nginx/conf.d/default.conf
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const api_url = `${window.location.protocol}//${window.location.hostname}:8888`;
+export const api_url = `https://blue.catbus.co.uk/api`;
 export const redirect_uri = `${window.location.origin}/login`;
 export const discordURL = `https://discordapp.com/oauth2/authorize?client_id=520030871256956928&redirect_uri=${encodeURIComponent(redirect_uri)}&response_type=code&scope=identify`;
 export const page_size = 20;
