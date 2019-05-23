be7062860bd1c752fa84467277e3ad31b20e5495
Blue <redacted>
2019-05-23 03:46:32 +0100

    Revert "Fix config"
    This reverts commit 9b61af901636d8e4ca02653120b93e2a96b797f9.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const api_url = `http://localhost:8888`;
+export const api_url = `http://127.0.0.1:8888`;
 export const redirect_uri = `${window.location.origin}/login`;
-export const discordURL = `https://discordapp.com/api/oauth2/authorize?client_id=561541673750888481&redirect_uri=${encodeURIComponent(redirect_uri)}&response_type=code&scope=identify`;
+export const discordURL = `https://discordapp.com/oauth2/authorize?client_id=561541673750888481&redirect_uri=${encodeURIComponent(redirect_uri)}&response_type=code&scope=identify`;
 export const inviteURL = "https://discordapp.com/api/oauth2/authorize?client_id=559426966151757824&permissions=537209856&scope=bot";
