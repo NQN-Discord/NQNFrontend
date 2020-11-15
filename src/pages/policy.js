4c60d8594536e0000560c26725801dce6513dffc
Blue <blue@nqn.blue>
2020-11-15 16:15:47 +0000

    Update policy

--- src/pages/policy.js
+++ src/pages/policy.js
@@ -10,6 +10,10 @@ export default function PrivacyPolicy() {
       </h1>
 
       <p>
+        This privacy policy is split in two parts, one governing the usage of information on Discord, the other for
+        this website.
+      </p>
+      <p>
         Blue is developing Not Quite Nitro as a free application. This Service is provided by Blue at no cost and is
         intended for use as is.
       </p>
@@ -18,6 +22,10 @@ export default function PrivacyPolicy() {
         policy. The Personal Information that is collected is used for providing and improving the Service. Your
         information is not shared except as described in this Privacy Policy.
       </p>
+      <hr/>
+      <h1>
+        On the Discord platform
+      </h1>
       <h2>
         Information Collection and Use
       </h2>
@@ -89,6 +97,307 @@ export default function PrivacyPolicy() {
         of the Service, message data, and other statistics. This Log Data is uploaded to Sentry.
       </p>
 
+      <hr/>
+      <h1>
+        The nqn.blue website
+      </h1>
+      <h2>
+        Information Collection and Use
+      </h2>
+
+      <h3>Third Parties</h3>
+      <p>
+        The website uses third party services that may collect information used to identify you.
+      </p>
+      <p>
+        Links to privacy policy of third party service providers used by the website:
+      </p>
+      <ul>
+        <li><a href="https://sentry.io/privacy/">Sentry</a></li>
+        <li><a href="https://policies.google.com/privacy">Google Analytics</a></li>
+        <li><a href="https://nitropay.com/privacy">Nitropay</a></li>
+      </ul>
+      <h4>Log Data</h4>
+      <p>
+        Whenever you use the Service, in a case of an error in the application, data and information is collected
+        (through third party products) called Log Data.
+        This Log Data may include information such as your device Internet Protocol (“IP”) address, device name,
+        operating system version, the configuration of the app when utilizing my Service, the time and date of your use
+        of the Service, message data, and other statistics. This Log Data is uploaded to Sentry.
+      </p>
+
+      <div>
+        <h2>
+          For California Residents: CCPA Information - nqn.blue website ONLY
+        </h2>
+        <p> This Privacy Notice for California Residents supplements the information contained elsewhere in this Privacy
+          Notice and applies solely to all visitors, users, and others who reside in the State of California
+          (“consumers” or “you”). We adopt this notice to comply with the California Consumer Privacy Act of 2018
+          (CCPA) and any terms defined in the CCPA have the same meaning when used in this notice.
+        </p>
+        <h2>
+          Rights and Choices
+        </h2>
+        <p>
+          The CCPA provides consumers located in the state of California with certain rights regarding their personal
+          information and data. The following section describes those rights and explains how to exercise them:
+        </p>
+        <h2>
+          Access to Specific Information and Data Portability Rights
+        </h2>
+        <p>
+          You have the right to request that I disclose certain information to you about our collection and
+          use of your personal information over the past 12 months. Once we receive and confirm your verifiable consumer
+          request (as described in the section “Exercising Access, Data Portability, and Deletion Rights”), we will
+          disclose to you:
+        </p>
+        <ul>
+          <li>The categories of personal information we collected about you.</li>
+          <li>The categories of sources for the personal information we collected about you.</li>
+          <li>Our business or commercial purpose for collecting or selling that personal
+            information.
+          </li>
+          <li>The categories of third parties with whom we share that personal information.</li>
+          <li> The specific pieces of personal information we collected about you (also called data
+            portability request)
+          </li>
+          <li>
+            If we sold or disclose your personal information for a business purpose, two separate lists disclosing:
+            <ul>
+              <li> Sales, identifying the personal information categories that each category of
+                recipient purchased, and
+              </li>
+              <li> Disclosures for a business purpose, identifying the personal information
+                categories that each category of recipient obtained
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <h2>Non-Discrimination</h2>
+        <p>
+          We will not discriminate against you for exercising any of your CCPA rights. Unless permitted by the CCPA,
+          we will not:
+        </p>
+        <ul>
+          <li>
+            Deny you goods or services.
+          </li>
+          <li>
+            Charge you different prices or rates for goods or services, including through granting discounts or imposing
+            penalties.
+          </li>
+          <li>
+            Provide you a different level or quality of goods or services.
+          </li>
+          <li>
+            Suggest that you may receive a different price or rate for goods or services or a different level of quality
+            of goods or services.
+          </li>
+        </ul>
+        <p>
+          Any CCPA-permitted financial incentive we offer will reasonably relate to your value
+          and contain written terms that describe the program’s material aspects.
+        </p>
+        <h2>Exercising Access, Data Portability, and Deletion Rights</h2>
+        <p>
+          To exercise the access, data portability, and deletion rights described above, please submit a verifiable
+          consumer request to us by emailing us at <a href="mailto:blue@nqn.blue">blue@nqn.blue</a>
+        </p>
+        <p>
+          Only you, or a person registered with the California Secretary of State that you authorize to act on your
+          behalf, may make a verifiable consumer request related to your personal information. You may also make a
+          verifiable consumer request on behalf of your minor child.
+        </p>
+        <p>
+          You may only make a verifiable consumer request for access of data portability twice within a 12-month period.
+          The verifiable consumer request must:
+        </p>
+        <ul>
+          <li> Provide sufficient information that allows us to reasonably verify you are the
+            person about whom we collected personal information or an authorized representative.
+          </li>
+          <li>
+            Describe your request with sufficient detail that allows us to properly understand, evaluate, and respond to it.
+          </li>
+        </ul>
+        <p>
+          We cannot respond to your request or provide you with personal information if we cannot
+          verify your identity or authority to make the request and confirm the personal information relates to
+          you.
+        </p>
+        <p>
+          Making a verifiable consumer request does not require you to create an
+          account with us. We will only use personal information provided in a verifiable consumer request to verify the
+          requestor’s identity or authority to make the request.
+        </p>
+        <h2>Information We Collect</h2>
+        <p> Our websites, emails (with your consent, where required by law), and
+          other products, services and platforms collect information that identifies, relates to, describes, references,
+          is capable of being associated with, or could reasonably be linked, directly or indirectly, with a particular
+          consumer or device (“personal information”). In particular, our websites, apps, emails, and other products,
+          services and platforms may have collected the following categories of personal information from its consumers
+          within the last twelve (12) months:
+        </p>
+        <table>
+          <tr>
+            <th>Category</th>
+            <th>Collected</th>
+          </tr>
+          <tr>
+            <td>A. Identifiers</td>
+            <td>YES</td>
+          </tr>
+          <tr>
+            <td> B. Personal information categories listed in the California Customer Records
+              statute (Cal. Civ. Code § 1798.80(e))
+            </td>
+            <td>YES</td>
+          </tr>
+          <tr>
+            <td>C. Protected classification characteristics under California or federal law</td>
+            <td>NO</td>
+          </tr>
+          <tr>
+            <td>D. Commercial information.</td>
+            <td>YES</td>
+          </tr>
+          <tr>
+            <td>E. Biometric information.</td>
+            <td>NO</td>
+          </tr>
+          <tr>
+            <td>F. Internet or other similar network activity.</td>
+            <td>YES</td>
+          </tr>
+          <tr>
+            <td>G. Geolocation data.</td>
+            <td>YES</td>
+          </tr>
+          <tr>
+            <td>H. Sensory data.</td>
+            <td>NO</td>
+          </tr>
+          <tr>
+            <td>I. Professional or employment-related information.</td>
+            <td>NO</td>
+          </tr>
+          <tr>
+            <td>J. Inferences drawn from other personal information.</td>
+            <td>YES</td>
+          </tr>
+        </table>
+        <p>Personal information does not include:</p>
+        <ul>
+          <li>Publicly available information from government records.</li>
+          <li>Deidentified or aggregated consumer information.</li>
+          <li> Information excluded from the CCPA’s scope, like: <ul>
+            <li>
+              health or medical information covered by the Health Insurance Portability and
+              Accountability Act of 1996 (HIPAA) and the California Confidentiality of Medical Information Act (CMIA) or
+              clinical trial data;
+            </li>
+            <li>
+              personal information covered by certain sector-specific privacy laws, including
+              the Fair Credit Reporting Act (FRCA), the Gramm-Leach-Bliley Act (GLBA) or California Financial
+              Information Privacy Act (FIPA), and the Driver’s Privacy.
+            </li>
+          </ul>
+          </li>
+        </ul>
+        <h2>Use of Personal Information</h2>
+        <p>
+          We may use or disclose the personal information we collect for one or more of the following business purposes:
+        </p>
+        <ul>
+          <li>
+            To fulfill or meet the reason you provided the information. For example, if you
+            share your name and contact information to ask a question about our products or services, we will use that
+            personal information to respond to your inquiry. If you provide your personal information to purchase a service,
+            we or our third-party service providers will use that information to process your payment and facilitate
+            delivery. We may also save your information to facilitate new product or service orders and requests.
+          </li>
+          <li>
+            To provide you with support and to respond to your inquiries, including
+            investigating and addressing your concerns and monitoring and improving our responses.
+          </li>
+          <li>
+            To personalize your website experience and to deliver content and product and service offerings relevant to
+            your interests, including targeted offers and ads through our website.
+          </li>
+          <li>
+            For testing, research, analysis, and product development, including to develop and
+            improve our websites, apps and other products, services and platforms.
+          </li>
+          <li>
+            To respond to law enforcement requests and as required by applicable law, court order, or governmental
+            regulations.
+          </li>
+          <li>
+            As described to you when collecting your personal information or as otherwise set forth in the CCPA.
+          </li>
+          <li>
+            To evaluate or conduct a merger, divestiture, restructuring, reorganization, dissolution, or other sale
+            or transfer of some or all of my assets, whether as a going concern or as part of bankruptcy, liquidation,
+            or similar proceeding, in which personal information held by me about my users is among the assets
+            transferred.
+          </li>
+        </ul>
+        <p>
+          I will not collect additional categories of personal information or use the personal information we collected
+          for materially different, unrelated, or incompatible purposes without providing you notice.
+        </p>
+        <h2>Sharing Personal Information</h2>
+        <p>
+          I may disclose your personal information to a third-party for a business purpose or sell your personal
+          information, subject to your right to opt-out of those sales (see ‘Sales of Personal Information’ below).
+          When we disclose personal information for a business purpose, we enter a contract that describes the purpose
+          and requires the recipient to both keep that personal information confidential and not use it for any purpose
+          except performing the contract. The CCPA prohibits third parties who purchase the personal information we hold
+          from reselling it unless you have received explicit notice and an opportunity to opt-out of further sales
+          (see ‘Sales of Personal Information’ below).
+        </p>
+        <p>
+          We may share your personal information with the following categories of third parties:
+        </p>
+        <ul>
+          <li>Subsidiaries and affiliates.</li>
+          <li>Contractors and service providers.</li>
+          <li>Data aggregators.</li>
+          <li>Third parties with whom we partner to offer products and services to you.</li>
+        </ul>
+        <h2>Disclosures of Personal Information for a Business Purpose</h2>
+        <p>
+          In the preceding twelve (12) months, I have disclosed the following categories of personal information for a
+          business purpose:
+        </p>
+        <ul>
+          <li>[Category A: Identifiers.]</li>
+          <li>[Category B: California Customer Records personal information categories.]</li>
+          <li>[Category F: Internet or other similar network activity.]</li>
+          <li>[Category G: Geolocation Data.]</li>
+          <li>[Category K: Inferences drawn from other personal information.]</li>
+        </ul>
+        <h2>Sales of Personal Information</h2>
+        <p>
+          In the preceding twelve (12) months, I have sold the following categories of personal information
+          collected through our ad-supported services:
+        </p>
+        <ul>
+          <li>[Category A: Identifiers.]</li>
+          <li>[Category F: Internet or other similar network activity.]</li>
+          <li>[Category G: Geolocation Data.]</li>
+          <li>[Category K: Inferences drawn from other personal information.]</li>
+        </ul>
+        <p>
+          Our advertising partners and I collect the personal information identified above (such as the cookies stored
+          on your browser, the advertising identifier on your mobile device, or the IP address of your device) when you
+          visit our website. We, and our partners, use this information to tailor and deliver ads to you on our website,
+          or to help tailor ads to you when you visit others’ sites (or use others’ apps). To tailor ads that may be more
+          relevant to you, we and/or our partners may share the information we collect with third parties.
+          <span data-ccpa-link="1"/>
+        </p>
+      </div>
+
       <h2>Cookies</h2>
       <p>
         Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are
@@ -99,13 +408,17 @@ export default function PrivacyPolicy() {
         choose to refuse our cookies, you may not be able to use some portions of this Service.
       </p>
 
+      <hr/>
+      <h1>
+        All services
+      </h1>
       <h2>Security</h2>
       <p>
         All data used by the service is stored using encryption at rest. We only allow access to the servers we run the
         service on to developers of the service, and we protect access by forcing the use of keys when accessing the
         servers. Physical access to the servers is also limited. If you find a security issue with any portion of the
-        service, please email me at blue@nqn.blue or send me a Direct Message on Discord. DO NOT post the issue publicly
-        before we have fixed it.
+        service, please email me at <a href="mailto:blue@nqn.blue">blue@nqn.blue</a> or send me a Direct Message on
+        Discord. DO NOT post the issue publicly before we have fixed it.
       </p>
 
       <h2>Data Removal</h2>
@@ -130,13 +443,14 @@ export default function PrivacyPolicy() {
       <p>
         I may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for
         any changes. I will notify you of any changes by posting the new Privacy Policy on this page. This policy is
-        effective as of 2020-11-11.
+        effective as of 2020-11-15.
       </p>
 
       <h2>Contact Us</h2>
       <p>
-        If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me at
-        blue@nqn.blue. I am also available in the Not Quite Nitro Discord server at <a href="https://discord.gg/e6pQupV">https://discord.gg/e6pQupV</a>
+        If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me
+        at <a href="mailto:blue@nqn.blue">blue@nqn.blue</a>. I am also available in the Not Quite Nitro Discord server
+        at <a href="https://discord.gg/e6pQupV">https://discord.gg/e6pQupV</a>
       </p>
     </Container>
   );
