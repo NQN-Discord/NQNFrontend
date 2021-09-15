46e55bf34450f41c218090d43c62d5ce84842d10
Blue <blue@nqn.blue>
2021-09-15 20:02:31 +0100

    Add some lazy loading

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Suspense, lazy} from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -10,29 +10,30 @@ import Alert from 'react-s-alert';
 
 import 'react-s-alert/dist/s-alert-default.css';
 import 'react-s-alert/dist/s-alert-css-effects/stackslide.css';
+import 'fomantic-ui-less/semantic.less';
 
 import {readStorageState, setRefreshToken} from "./actions/auth";
 import {fetchEmotes} from "./actions/user";
 import {fetchGuilds} from "./actions/guild";
 
-import WebhookPage from "./pages/guild_dashboard/server_view";
-import ManagerRootPage from "./pages/emote_manager/manager_root";
-import LoginPage from "./pages/login";
-import InvitePage from "./pages/invite";
 import {Header, BottomFooter} from "./header";
 import {HelpTextPage} from "./components/helpText";
-import UserFeedback from "./pages/feedback";
-import PrivacyPolicy from "./pages/policy";
-import GuildCreatorPage from "./pages/guild_builder/guild_builder";
-import GuildSelectorPage from "./pages/guild_builder/guild_selector";
-import GuildStatusPage from "./pages/guild_builder/guild_status";
-import LicensePage from "./pages/licenses";
-import CommandPage from "./pages/commands";
-import BotAddedPage from "./pages/bot_added";
-import PremiumPage from "./pages/premium";
 
-import 'fomantic-ui-less/semantic.less';
-import HomePage from "./pages/home";
+
+const WebhookPage = lazy(() => import("./pages/guild_dashboard/server_view"));
+const ManagerRootPage = lazy(() => import("./pages/emote_manager/manager_root"));
+const LoginPage = lazy(() => import("./pages/login"));
+const InvitePage = lazy(() => import("./pages/invite"));
+const UserFeedback = lazy(() => import("./pages/feedback"));
+const PrivacyPolicy = lazy(() => import("./pages/policy"));
+const GuildCreatorPage = lazy(() => import("./pages/guild_builder/guild_builder"));
+const GuildSelectorPage = lazy(() => import("./pages/guild_builder/guild_selector"));
+const GuildStatusPage = lazy(() => import("./pages/guild_builder/guild_status"));
+const LicensePage = lazy(() => import("./pages/licenses"));
+const CommandPage = lazy(() => import("./pages/commands"));
+const BotAddedPage = lazy(() => import("./pages/bot_added"));
+const PremiumPage = lazy(() => import("./pages/premium"));
+const HomePage = lazy(() => import("./pages/home"));
 
 class App extends Component {
   componentDidMount() {
@@ -72,50 +73,52 @@ class App extends Component {
           <div className="site_body">
             <Header/>
             <div className="site_container">
-              {!this.props.loggedIn &&
-                <Switch>
-                  <Route exact path="/" component={HomePage}/>
-                  <Route exact path="/help" component={HelpTextPage}/>
-                  <Route exact path="/privacy" component={PrivacyPolicy}/>
-                  <Route exact path="/licenses" component={LicensePage}/>
-                  <Route exact path="/commands" component={CommandPage}/>
-                  <Route exact path="/bot_added" component={BotAddedPage}/>
-                  <Route exact path="/invite" component={InvitePage}/>
-                  <LoginPage/>
-                </Switch>
-              }
-              {this.props.loggedIn &&
-                <Switch>
-                  <Route exact path="/guilds/:guildID" component={WebhookPage}/>
-                  <Route exact path="/guilds/:guildID/channels/:channelID" component={WebhookPage}/>
-                  <Route exact path="/guilds/" component={WebhookPage}/>
-                  <Route exact path="/guilds/:guildID/:page" component={WebhookPage}/>
-
-                  <Route exact path="/emote_manager/overview" component={ManagerRootPage}/>
-                  <Route exact path="/emote_manager/packs" component={ManagerRootPage}/>
-                  <Route exact path="/emote_manager/packs/search" component={ManagerRootPage}/>
-                  <Route exact path="/emote_manager/alias" component={ManagerRootPage}/>
-                  <Route exact path="/emote_manager/alias/search" component={ManagerRootPage}/>
-
-                  <Route exact path="/login" component={LoginPage}/>
-                  <Route exact path="/invite" component={InvitePage}/>
-
-                  <Route exact path="/guild_builder" component={GuildCreatorPage}/>
-                  <Route exact path="/guild_builder/selector" component={GuildSelectorPage}/>
-                  <Route exact path="/guild_builder/status" component={GuildStatusPage}/>
-
-                  <Route exact path="/reference" component={ManagerRootPage}/>
-
-                  <Route exact path="/" component={HomePage}/>
-                  <Route exact path="/help" component={HelpTextPage}/>
-                  <Route exact path="/feedback" component={UserFeedback}/>
-                  <Route exact path="/privacy" component={PrivacyPolicy}/>
-                  <Route exact path="/licenses" component={LicensePage}/>
-                  <Route exact path="/commands" component={CommandPage}/>
-                  <Route exact path="/bot_added" component={BotAddedPage}/>
-                  <Route exact path="/premium" component={PremiumPage}/>
-                </Switch>
-              }
+              <Suspense fallback={<div>Loading...</div>}>
+                {!this.props.loggedIn &&
+                  <Switch>
+                    <Route exact path="/" component={HomePage}/>
+                    <Route exact path="/help" component={HelpTextPage}/>
+                    <Route exact path="/privacy" component={PrivacyPolicy}/>
+                    <Route exact path="/licenses" component={LicensePage}/>
+                    <Route exact path="/commands" component={CommandPage}/>
+                    <Route exact path="/bot_added" component={BotAddedPage}/>
+                    <Route exact path="/invite" component={InvitePage}/>
+                    <LoginPage/>
+                  </Switch>
+                }
+                {this.props.loggedIn &&
+                  <Switch>
+                    <Route exact path="/guilds/:guildID" component={WebhookPage}/>
+                    <Route exact path="/guilds/:guildID/channels/:channelID" component={WebhookPage}/>
+                    <Route exact path="/guilds/" component={WebhookPage}/>
+                    <Route exact path="/guilds/:guildID/:page" component={WebhookPage}/>
+  
+                    <Route exact path="/emote_manager/overview" component={ManagerRootPage}/>
+                    <Route exact path="/emote_manager/packs" component={ManagerRootPage}/>
+                    <Route exact path="/emote_manager/packs/search" component={ManagerRootPage}/>
+                    <Route exact path="/emote_manager/alias" component={ManagerRootPage}/>
+                    <Route exact path="/emote_manager/alias/search" component={ManagerRootPage}/>
+  
+                    <Route exact path="/login" component={LoginPage}/>
+                    <Route exact path="/invite" component={InvitePage}/>
+  
+                    <Route exact path="/guild_builder" component={GuildCreatorPage}/>
+                    <Route exact path="/guild_builder/selector" component={GuildSelectorPage}/>
+                    <Route exact path="/guild_builder/status" component={GuildStatusPage}/>
+  
+                    <Route exact path="/reference" component={ManagerRootPage}/>
+  
+                    <Route exact path="/" component={HomePage}/>
+                    <Route exact path="/help" component={HelpTextPage}/>
+                    <Route exact path="/feedback" component={UserFeedback}/>
+                    <Route exact path="/privacy" component={PrivacyPolicy}/>
+                    <Route exact path="/licenses" component={LicensePage}/>
+                    <Route exact path="/commands" component={CommandPage}/>
+                    <Route exact path="/bot_added" component={BotAddedPage}/>
+                    <Route exact path="/premium" component={PremiumPage}/>
+                  </Switch>
+                }
+              </Suspense>
             </div>
           </div>
           <BottomFooter/>
