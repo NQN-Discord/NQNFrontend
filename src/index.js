db76da2fd3828b9fef10f853a20f4801f9d9fab2
Blue <blue@nqn.blue>
2021-09-16 20:35:31 +0100

    Move sentry to lazy load

--- src/index.js
+++ src/index.js
@@ -3,25 +3,17 @@ import ReactDOM from 'react-dom';
 import thunkMiddleware from 'redux-thunk';
 import {applyMiddleware, compose, createStore} from "redux";
 import { Provider } from 'react-redux';
-import * as Sentry from '@sentry/react';
-
 import rootReducer from "./reducers";
 import App from "./main";
 import './nitropay.css';
-import {sentryDns} from "./config";
 
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-if (sentryDns !== "") {
-  Sentry.init({dsn: sentryDns});
-}
 
 const store = createStore(
   rootReducer,
   composeEnhancer(
     applyMiddleware(thunkMiddleware),
-    Sentry.createReduxEnhancer({})
   )
 );
 
