b5d0c994d79c150d14366865d83d8f2c742621b2
Blue <blue@nqn.blue>
2021-11-05 22:30:43 +0000

    Disable ads

--- src/components/ad.js
+++ src/components/ad.js
@@ -1,71 +1,8 @@
 import React, {Component} from 'react';
-import {adDemoMode} from '../config.js';
-
-let adsStarted = false;
 
 
 export default class Ad extends Component {
-  componentDidMount() {
-    const hasAds = window['nitroAds'] !== undefined;
-    if (hasAds) {
-      this.createAd();
-    } else if (adsStarted) {
-      // Load has started but we've not got the script yet
-      const listener = () => {
-        document.removeEventListener("nitroAds.loaded", listener);
-        this.createAd();
-      };
-      document.addEventListener("nitroAds.loaded", listener);
-    } else {
-      adsStarted = true;
-
-      const script = document.createElement('script');
-      script.src = "https://s.nitropay.com/ads-580.js";
-      script.onload = () => {
-        window["nitroAds"] = window["nitroAds"] || {
-          createAd: function() {
-            window.nitroAds.queue.push(["createAd", arguments]);
-          },
-          queue: []
-        };
-        this.createAd();
-      };
-      script.setAttribute("async", "");
-
-      document.head.appendChild(script);
-    }
-  }
-
-  createAd() {
-    console.log(`Creating ad for ${this.props.id}. Format: ${this.props.format}`);
-    const ad = window['nitroAds'].createAd(this.props.id, {
-      "demo": adDemoMode,
-      "refreshLimit": 10,
-      "refreshTime": 90,
-      "renderVisibleOnly": true,
-      "refreshVisibleOnly": true,
-      "sizes": this.props.sizes,
-      "format": this.props.format,
-      "anchor": "bottom",
-      "mediaQuery": this.props.format === "anchor" && "(max-width: 992px)",
-      "report": {
-        "enabled": true,
-        "wording": "Report Ad",
-        "position": "top-right"
-      }
-    });
-    this.setState({ad});
-  }
-
-
-  componentWillUnmount() {
-    console.log(`Unloading ad for ${this.props.id}`);
-    if (this.state && this.state.ad) {
-      this.state.ad.onNavigate();
-    }
-  }
-
   render() {
-    return <div id={this.props.id} style={this.props.style || {}}/>;
+    return <></>;
   }
 }
