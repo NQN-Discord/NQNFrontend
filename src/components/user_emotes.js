bf908900d05a2cd8faf6e37aacf54587c1a9e305
Blue <blue@nqn.blue>
2022-09-17 20:54:10 +0100

    Migrate some components to functional style

--- src/components/user_emotes.js
+++ src/components/user_emotes.js
@@ -9,55 +9,54 @@ import connect from "react-redux/es/connect/connect";
 import {Emote} from "./emote";
 
 
-class UserEmotes extends Component {
-  renderEmotes(title, emotes, names) {
-    const emoteList = (
-      emotes.filter(emote => {
-        if (names.has(emote.name)) {
-          return false;
-        } else if (this.props.filter && !this.props.filter(emote)) {
-          return false
-        }
-        names.add(emote.name);
-        return true;
-      }).map(emote => {
-        const e = new Emote(emote);
-        return e.renderImg(() => this.props.onClick(e))
-      })
-    );
-    if (emoteList.length === 0) {
-      return
-    }
-    return {key: title, title: [
-        title,
-        <div key="div" style={{display: "inline", padding: "0.5rem"}}/>,
-        emoteList.slice(0, 5)
-      ], content: emoteList}
+const UserEmotes = (props) => {
+  const names = new Set();
+
+  const pack_emotes = Object.entries(props.packs).map(([name, {emotes, is_public}]) => renderEmotes(props, name, emotes, names));
+  const guild_emotes = Object.entries(props.guilds).map(
+    ([id, {name}]) => renderEmotes(props, name, (props.guild_emotes[id] || []).concat(props.guild_aliases[id] || []), names)
+  );
+
+  const panels = [
+    renderEmotes(props, "Aliases", props.user_aliases, names),
+    ...pack_emotes.sort((a, b) => a.key.localeCompare(b.key)),
+    ...guild_emotes.sort((a, b) => a.key.localeCompare(b.key)),
+  ].filter(e => e);
+  return (
+    <Accordion
+      defaultActiveIndex={[0]}
+      panels={panels}
+      exclusive={false}
+      fluid
+    />
+  );
+};
+
+const renderEmotes = (props, title, emotes, names) => {
+  const emoteList = (
+    emotes.filter(emote => {
+      if (names.has(emote.name)) {
+        return false;
+      } else if (props.filter && !props.filter(emote)) {
+        return false
+      }
+      names.add(emote.name);
+      return true;
+    }).map(emote => {
+      const e = new Emote(emote);
+      return e.renderImg(() => props.onClick(e))
+    })
+  );
+  if (emoteList.length === 0) {
+    return
   }
+  return {key: title, title: [
+      title,
+      <div key="div" style={{display: "inline", padding: "0.5rem"}}/>,
+      emoteList.slice(0, 5)
+    ], content: emoteList}
+};
 
-  render() {
-    const names = new Set();
-
-    const pack_emotes = Object.entries(this.props.packs).map(([name, {emotes, is_public}]) => this.renderEmotes(name, emotes, names));
-    const guild_emotes = Object.entries(this.props.guilds).map(
-      ([id, {name}]) => this.renderEmotes(name, (this.props.guild_emotes[id] || []).concat(this.props.guild_aliases[id] || []), names)
-    );
-
-    const panels = [
-      this.renderEmotes("Aliases", this.props.user_aliases, names),
-      ...pack_emotes.sort((a, b) => a.key.localeCompare(b.key)),
-      ...guild_emotes.sort((a, b) => a.key.localeCompare(b.key)),
-    ].filter(e => e);
-    return (
-      <Accordion
-        defaultActiveIndex={[0]}
-        panels={panels}
-        exclusive={false}
-        fluid
-      />
-    );
-  }
-}
 
 const mapStateToProps = (state) => {
   return {
