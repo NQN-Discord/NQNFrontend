10c13730a03d05f67936cef2eddbbd3690af9200
Blue <blue@nqn.blue>
2024-08-25 11:24:18 +0100

    Docker entrypoint with env substitution

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
-{ cat src/index.html envsubst } > src/index.html
+export USE_ENV_VARIABLES="true"
+
+{ envsubst < /usr/share/nginx/html/index.html; } > /usr/share/nginx/html/index.html.new
+mv /usr/share/nginx/html/index.html.new /usr/share/nginx/html/index.html
 
 nginx -g daemon off;
